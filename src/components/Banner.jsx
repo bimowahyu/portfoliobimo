03d4ed4d58324f4850ef3d21c6d2fd390cb6c048
Bimo Wahyu Syahputro <128451452+bimowahyu@users.noreply.github.com>
2025-08-27 20:06:00 +0700

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Bimo Wahyu`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>I'm from Semarang,<br></br>graduated from vocational school in computer and network engineering and have several projects</p>
+                  <p>I'm from Semarang,<br></br>graduated bachelor of information systems and have several projects</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
